d63ae7e7c33f67229ca222fb077b4056302e37cd
Philip Teow <philipteow@Philips-MacBook-Pro.local>
2018-02-02 11:06:50 +0800

    initial commit

---
+++ ThirdAssesment_PhilipTeow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThirdAssesment_PhilipTeow
+//
+//  Created by Philip Teow on 02/02/2018.
+//  Copyright © 2018 Philip Teow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
